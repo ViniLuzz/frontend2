81383a5145bb8e94467bd127084c76c8192b55c4
ViniLuzz <129405100+ViniLuzz@users.noreply.github.com>
2025-05-28 09:15:09 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { Routes, Route} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from './store';
-import { goToStep } from './store/stepsSlice';
 import UploadScreen from './components/UploadScreen';
 import ClauseExplanationScreen from './components/ClauseExplanationScreen';
 import SuccessScreen from './components/SuccessScreen';
@@ -13,22 +11,24 @@ import RegisterScreen from './components/RegisterScreen';
 import LoginScreen from './components/LoginScreen';
 import HomeScreen from './components/HomeScreen';
 import AnalysesHistoryScreen from './components/AnalysesHistoryScreen';
+import { useEffect } from 'react';
+import { loginSuccess } from './store/authSlice';
+import { auth } from './firebase';
 
 const App = () => {
   const step = useSelector((state: RootState) => state.steps.step);
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
   const user = useSelector((state: RootState) => state.auth.user);
   const dispatch = useDispatch();
-  const location = useLocation();
 
   useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const sessionId = params.get('session_id');
-    if (sessionId) {
-      localStorage.setItem('pago', 'true');
-      dispatch(goToStep(5));
-    }
-  }, [location, dispatch]);
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(loginSuccess({ email: user.email || '' }));
+      }
+    });
+    return () => unsubscribe();
+  }, [dispatch]);
 
   return (
     <>
