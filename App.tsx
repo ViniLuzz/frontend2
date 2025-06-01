497dd5660b3a790c7c156cc70f4690c8ea0f7376
ViniLuzz <129405100+ViniLuzz@users.noreply.github.com>
2025-06-01 19:26:51 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,64 @@
+import { Routes, Route} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from './store';
+import UploadScreen from './components/UploadScreen';
+import ClauseExplanationScreen from './components/ClauseExplanationScreen';
+import SuccessScreen from './components/SuccessScreen';
+import CancelScreen from './components/CancelScreen';
+import ProfileMenu from './components/ProfileMenu';
+import SummaryScreen from './components/SummaryScreen';
+import RegisterScreen from './components/RegisterScreen';
+import LoginScreen from './components/LoginScreen';
+import HomeScreen from './components/HomeScreen';
+import AnalysesHistoryScreen from './components/AnalysesHistoryScreen';
+import { useEffect } from 'react';
+import { loginSuccess } from './store/authSlice';
+import { auth } from './firebase';
+import PaymentStep from './components/PaymentStep';
+
+
+const App = () => {
+  const step = useSelector((state: RootState) => state.steps.step);
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  const user = useSelector((state: RootState) => state.auth.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(loginSuccess({ email: user.email || '' }));
+      }
+    });
+    return () => unsubscribe();
+  }, [dispatch]);
+
+  return (
+    <>
+      
+      <Routes>
+      <Route path="/cancel" element={<div className="app-bg"><CancelScreen /></div>} />
+      <Route path="/success" element={<div className="app-bg"><SuccessScreen /></div>} />
+         {isAuthenticated && user && <ProfileMenu user={user} />}
+        <Route path="/register" element={<div className="app-bg"><RegisterScreen /></div>} />
+        <Route path="/login" element={<div className="app-bg"><LoginScreen /></div>} />
+        <Route path="/analises" element={<div className="app-bg"><AnalysesHistoryScreen /></div>} />
+        <Route path="/upload" element={<div className="app-bg"><UploadScreen /></div>} />
+        <Route path="/pagamento" element={<div className="app-bg"><PaymentStep /></div>} />
+        <Route path="/clausulas" element={<div className="app-bg"><ClauseExplanationScreen /></div>} />
+        <Route path="/analise" element={<div className="app-bg"><SummaryScreen /></div>} />
+        <Route path="/" element={
+          <div className="app-bg">
+            {step === 1 && <HomeScreen />}
+            {step === 2 && <UploadScreen />}
+            {step === 3 && <PaymentStep />}
+            {step === 4 && <ClauseExplanationScreen />}
+            {step === 5 && <SummaryScreen />}
+            {step === 6 && <SuccessScreen />}
+          </div>
+        } />
+      </Routes>
+    </>
+  );
+};
+
+export default App; 
